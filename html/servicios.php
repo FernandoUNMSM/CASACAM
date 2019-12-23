7fe94a720158fc571c036d07b0425d44a34c6409
Oscar Fernando <43102959+FernandoUNMSM@users.noreply.github.com>
2019-12-22 21:06:20 -0500

    Add files via upload

---
+++ html/servicios.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Casacam</title>
+  <link rel="icon" href="../img/cascam-logo.png">
+  <link rel="stylesheet" href="../css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Bree+Serif|Poppins:400,900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <!-- Header -->
+  <header>
+    <nav class="container-menu menu-serv">
+      <div class="sombra">
+        <ul class="menu">
+          <li class="menu-servicios"><a class="list-item" href="">Nosotros</a></li class="menu-servicios">
+          <li class="menu-servicios"><a class="list-item" href="../index.php"
+              style="padding-left: 25px; padding-right: 25px;">Inicio</a></li>
+          <li class="menu-servicios"><a class="list-item" href="./contacto.php">Contactanos</a></li>
+        </ul>
+      </div>
+    </nav>
+
+  </header>
+  <!-- Header -->
+
+  <!-- Servicios INICIO -->
+  <section class="section-servicios">
+    <div class="sombra">
+      <h1><span>Descubre</span> lo que <span>necesitas</span></h1>
+      <article class="contenido-categorias">
+        <section class="categorias camaras-alarmas">
+          <a href="">
+            <section class="camaras">
+              <div class="camaras-titulo titu">
+                <div class="rotate1">
+                  <h2>CAMARAS</h2>
+                </div>
+              </div>
+              <article class="categoria-image-1"></article>
+            </section>
+          </a>
+          <a class="alarmas-a" href="">
+            <section class="alarmas">
+              <article class="categoria-image-2"></article>
+              <div class="alarmas-titulo titu">
+                <div class="rotate2">
+                  <h2>ALARMAS</h2>
+                </div>
+              </div>
+            </section>
+          </a>
+        </section>
+        <div class="categorias-inferior">
+          <section class="categorias gps-pozo-insta">
+            <a href="">
+              <section class="gps">
+                <article class="categoria-image-3"></article>
+                <div class="gps-titulo titu">
+                  <div class="rotate2">
+                    <h2>GPS</h2>
+                  </div>
+                </div>
+              </section>
+            </a>
+            <a href="">
+              <section class="pozo">
+                <div class="pozo-titulo titu">
+                  <div class="rotate1 esp">
+                    <h2 class="h2-esp">POZOS A TIERRA</h2>
+                  </div>
+                </div>
+                <article class="categoria-image-4"></article>
+              </section>
+            </a>
+            <a href="">
+              <section class="insta">
+                <article class="categoria-image-5"></article>
+                <div class="insta-titulo titu">
+                  <div class="rotate2">
+                    <h2>INSTALACIONES ELECTRICAS</h2>
+                  </div>
+                </div>
+              </section>
+            </a>
+          </section>
+          <section class="categorias fragancias pararrayos">
+            <a href="">
+              <article class="categoria-image-6"></article>
+              <div class="pararrayos-titulo titu">
+                <div class="rotate2">
+                  <h2>PARARRAYOS</h2>
+                </div>
+              </div>
+            </a>
+          </section>
+        </div>
+      </article>
+    </div>
+  </section>
+  <!-- Servicios FIN -->
+</body>
+
+</html>
